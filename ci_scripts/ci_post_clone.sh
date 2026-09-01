d1996c6fc7dbadacfd0f93f4f4dcfbb2443317dd
Apple <apple@Souravs-Macbook-Pro.local>
2026-09-02 00:16:03 +0530

    Fix Apple typo in IDESkipPackagePluginFingerprintValidation

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Disable package plugin fingerprint validation (with the Apple intentional typo: Validatation)
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+
+# Also include the correct spelling just in case Apple fixes the typo in the future
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidation -bool YES
+
+# Skip Macro Fingerprint Validation as well
+defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
